b08de3e2baeb945e0dd09037b3392c362d42ac75
SingSu05 <sumeet.singh@lexisnexisrisk.com>
2021-11-21 17:00:10 +0000

    Initial setup of mysite with settings.py setup, blueprint ready

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\singsu05\OneDrive - Reed Elsevier Group ICO Reed Elsevier Inc\Documents\MyFiles\August\nootnoot\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
